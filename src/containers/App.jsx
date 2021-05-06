80d502e9187aa840a9a557eb13bf2ab4f63acbb6
jorge <georgellanque@gmail.com>
2021-05-05 20:20:04 -0400

    Adding fake api

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
@@ -7,8 +7,22 @@ import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
+const App = () => {
+
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState')
+        .then(response => response.json())
+        .then(data => setVideos(data));
+    }, []);
+
+    console.log(videos);
+
+
+
+    return (
+        <div className="App">
         <Header />
         <Search />
         <Categories title="Mi Lista">
@@ -38,6 +52,7 @@ const App = () => (
         
         <Footer />
     </div>
-);
+    )
+}
 
 export default App;
